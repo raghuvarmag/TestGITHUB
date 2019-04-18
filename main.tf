6568f8f6f4d595ca56653b2bce2b750c72e5720f
Nagaraghuvarma Gottumukkala <Nagaraghuvarma_G@ad.infosys.com>
2019-04-18 12:26:21 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,10 @@
+# Configure the provider
+provider "azurerm" {
+    version = "=1.20.0"
+}
+
+# Create a new resource group
+resource "azurerm_resource_group" "rg" {
+    name     = "myTFResourceGroupDevNew1235"
+    location = "westus"
+}
