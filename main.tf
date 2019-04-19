5103c1c4b627ccfaee5e1682bba549d682ca3a53
raghuvarmag <raghuvarma.g@gmail.com>
2019-04-19 14:56:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ provider "azurerm" {
 # Create a new resource group
 resource "azurerm_resource_group" "rg" {
     name     = "myTFResourceGroupDevNew1235"
-    location = "Europe"
+    location = "WestIndia"
 }
